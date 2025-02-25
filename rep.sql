8d6fa644ba0d0eeaa5f68c229d34538a0ff28066
Yanis Litim <yanis.litim@etu.univ-orleans.fr>
2025-02-25 15:37:54 +0100

    Update rep.sql

--- rep.sql
+++ rep.sql
@@ -11,3 +11,15 @@ from LIVRE
 join DETAILCOMMANDE on DETAILCOMMANDE.isbn = LIVRE.isbn
 join COMMANDE on DETAILCOMMANDE.numcom = COMMANDE.numcom
 where COMMANDE.datecom = str_to_date('01/12/2024', '%d/%m/%Y');
+
+
+
+SELECT DISTINCT CLIENT.*
+FROM CLIENT
+JOIN COMMANDE ON COMMANDE.idcli = CLIENT.idcli
+JOIN DETAILCOMMANDE ON COMMANDE.numcom = DETAILCOMMANDE.numcom
+JOIN LIVRE ON DETAILCOMMANDE.isbn = LIVRE.isbn
+JOIN ECRIRE on ECRIRE.isbn = LIVRE.isbn
+join AUTEUR on ECRIRE.idauteur = AUTEUR.idauteur
+WHERE AUTEUR.nomauteur = 'René Goscinny' -- 
+    AND COMMANDE.datecom LIKE '2021%';
